598362f0465e4da5b459864eaea252260bc377a2
ItamarGouveia <contato.itamargouveia@gmail.com>
2022-08-08 08:45:52 -0300

    modulo 14

---
+++ src/br/com/itamar/dao/IClienteDAO.java
@@ -0,0 +1,21 @@
+package br.com.itamar.dao;
+
+import br.com.itamar.domain.Cliente;
+
+import java.util.Collection;
+
+/**
+ * @author rodrigo.pires
+ */
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
